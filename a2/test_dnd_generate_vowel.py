30b15b78aac4c906b4d6db4f2de7d6331c9a600a
Tokeybee <tokeybees@gmail.com>
2020-02-13 10:13:49 -0800

    created unit tests for generate vowel function

--- a2/test_dnd_generate_vowel.py
+++ a2/test_dnd_generate_vowel.py
@@ -4,4 +4,8 @@ import dnd
 
 class Test(TestCase):
     def test_generate_vowel(self):
-        self.fail()
+        for _ in range(100):
+            result = dnd.generate_vowel()
+            if result not in "aeiouy":
+                self.fail()
+        self.defaultTestResult()
