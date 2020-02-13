8763b4780a9a812f28f1cbf92194bd2e697dece8
Tokeybee <tokeybees@gmail.com>
2020-02-13 15:03:23 -0800

    tested select_class for wizard, removed irrelevant partitions.

--- a2/test_dnd_select_class.py
+++ a2/test_dnd_select_class.py
@@ -10,68 +10,14 @@ class Test(TestCase):
         actual = dnd.select_class()
         self.assertEqual(expected, actual)
 
-    @patch("builtins.input", side_effect=["2"])
-    def test_select_class_bard(self, _):
-        expected = "bard"
-        actual = dnd.select_class()
-        self.assertEqual(expected, actual)
-
-    @patch("builtins.input", side_effect=["3"])
-    def test_select_class_cleric(self, _):
-        expected = "cleric"
-        actual = dnd.select_class()
-        self.assertEqual(expected, actual)
-
-    @patch("builtins.input", side_effect=["4"])
-    def test_select_class_druid(self, _):
-        expected = "druid"
-        actual = dnd.select_class()
-        self.assertEqual(expected, actual)
-
-    @patch("builtins.input", side_effect=["5"])
-    def test_select_class_fighter(self, _):
-        expected = "fighter"
-        actual = dnd.select_class()
-        self.assertEqual(expected, actual)
-
-    @patch("builtins.input", side_effect=["6"])
-    def test_select_class_monk(self, _):
-        expected = "monk"
-        actual = dnd.select_class()
-        self.assertEqual(expected, actual)
-
     @patch("builtins.input", side_effect=["7"])
     def test_select_class_paladin(self, _):
         expected = "paladin"
         actual = dnd.select_class()
         self.assertEqual(expected, actual)
 
-    @patch("builtins.input", side_effect=["8"])
-    def test_select_class_ranger(self, _):
-        expected = "ranger"
-        actual = dnd.select_class()
-        self.assertEqual(expected, actual)
-
-    @patch("builtins.input", side_effect=["9"])
-    def test_select_class_rogue(self, _):
-        expected = "rogue"
-        actual = dnd.select_class()
-        self.assertEqual(expected, actual)
-
-    @patch("builtins.input", side_effect=["10"])
-    def test_select_class_sorcerer(self, _):
-        expected = "sorcerer"
-        actual = dnd.select_class()
-        self.assertEqual(expected, actual)
-
-    @patch("builtins.input", side_effect=["11"])
-    def test_select_class_warlock(self, _):
-        expected = "warlock"
-        actual = dnd.select_class()
-        self.assertEqual(expected, actual)
-
-    @patch("builtins.input", side_effect="1")
-    def test_select_class_barbarian(self, _):
-        expected = "barbarian"
+    @patch("builtins.input", side_effect=["12"])
+    def test_select_class_wizard(self, _):
+        expected = "wizard"
         actual = dnd.select_class()
         self.assertEqual(expected, actual)
