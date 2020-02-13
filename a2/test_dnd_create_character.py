0ebd4dc803a546f1b4976b138b50efbc6cf1ad8f
Tokeybee <tokeybees@gmail.com>
2020-02-13 14:34:27 -0800

    Made more unittests for create_character

--- a2/test_dnd_create_character.py
+++ a2/test_dnd_create_character.py
@@ -14,7 +14,7 @@ class Test(TestCase):
     @patch("dnd.select_class", side_effect=["monk"])
     @patch("dnd.select_race", side_effect=["elf"])
     @patch("dnd.roll_hitpoints", side_effect=[7])
-    def test_create_character_one_syllable(self, _, __, ___, ____, _____):
+    def test_create_character_two_syllables(self, _, __, ___, ____, _____):
         expected = {"Name": "Xyxy",
                     "Strength": 10,
                     "Dexterity": 8,
@@ -28,4 +28,25 @@ class Test(TestCase):
                     "Race": "elf",
                     "HP": [7, 7]}
         actual = dnd.create_character(2)
+        self.assertEqual(expected, actual)
+
+    @patch("dnd.roll_die", side_effect=[10, 8, 5, 9, 7, 3])
+    @patch("dnd.generate_name", side_effect=["Po"])
+    @patch("dnd.select_class", side_effect=["monk"])
+    @patch("dnd.select_race", side_effect=["elf"])
+    @patch("dnd.roll_hitpoints", side_effect=[7])
+    def test_create_character_one_syllable(self, _, __, ___, ____, _____):
+        expected = {"Name": "Po",
+                    "Strength": 10,
+                    "Dexterity": 8,
+                    "Intelligence": 5,
+                    "Charisma": 9,
+                    "Wisdom": 7,
+                    "Constitution": 3,
+                    "Inventory": [],
+                    "XP": 0,
+                    "Class": "monk",
+                    "Race": "elf",
+                    "HP": [7, 7]}
+        actual = dnd.create_character(1)
         self.assertEqual(expected, actual)
---
+++ a2/test_dnd_select_class.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from unittest.mock import patch
+import dnd
+
+
+class Test(TestCase):
+    def test_select_class(self):
+        self.fail()
