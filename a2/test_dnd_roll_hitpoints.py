0b725c48f1586082bebd29fe10ccf3e4317d0328
Tokeybee <tokeybees@gmail.com>
2020-02-13 16:54:53 -0800

    tested roll_hitpoints for max 8 hp

--- a2/test_dnd_roll_hitpoints.py
+++ a2/test_dnd_roll_hitpoints.py
@@ -5,8 +5,22 @@ import dnd
 
 class Test(TestCase):
     @patch("dnd.roll_die", side_effect=[12])
-    def test_roll_hitpoints(self, _):
+    def test_roll_hitpoints_max_12(self, _):
         character = {"Class": "barbarian"}
         expected = 12
+        actual = dnd.roll_hitpoints(character)
+        self.assertEqual(expected, actual)
+
+    @patch("dnd.roll_die", side_effect=[9])
+    def test_roll_hitpoints_max_10(self, _):
+        character = {"Class": "fighter"}
+        expected = 9
+        actual = dnd.roll_hitpoints(character)
+        self.assertEqual(expected, actual)
+
+    @patch("dnd.roll_die", side_effect=[6])
+    def test_roll_hitpoints_max_8(self, _):
+        character = {"Class": "rogue"}
+        expected = 6
         actual = dnd.roll_hitpoints(character)
         self.assertEqual(expected, actual)
