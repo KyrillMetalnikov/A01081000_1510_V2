e208772c316c727e91f00c8720411dbe2c52221a
Tokeybee <tokeybees@gmail.com>
2020-02-14 11:12:33 -0800

    tested single_attack for a successful hit and survival.

--- a2/test_dnd_combat_round.py
+++ a2/test_dnd_combat_round.py
@@ -1,6 +1,37 @@
 from unittest import TestCase
+from unittest.mock import patch
+import dnd
+import unittest
+import io
 
 
 class Test(TestCase):
-    def test_combat_round(self):
-        self.fail()
+    @patch("roll_for_initiative", side_effect=[True])
+    @patch()
+    def test_combat_round(self, _):
+        character = {"Name": "Xyxy",
+                     "Strength": 10,
+                     "Dexterity": 8,
+                     "Intelligence": 5,
+                     "Charisma": 9,
+                     "Wisdom": 7,
+                     "Constitution": 3,
+                     "Inventory": [],
+                     "XP": 0,
+                     "Class": "monk",
+                     "Race": "elf",
+                     "HP": [7, 7]}
+        sad_jim = {
+            "Name": "Sad Jim",
+            "Strength": 7,
+            "Dexterity": 8,
+            "Intelligence": 5,
+            "Charisma": 0,  # I'm aware this is impossible by normal means, but sad jim is just awful.
+            "Wisdom": 11,
+            "Constitution": 3,
+            "Inventory": [],
+            "XP": 0,
+            "Class": "loser",
+            "Race": "human",
+            "HP": [3, 3]
+        }
--- a2/test_dnd_single_attack.py
+++ a2/test_dnd_single_attack.py
@@ -1,6 +1,43 @@
 from unittest import TestCase
+from unittest.mock import patch
+import unittest
+import io
+import dnd
 
 
 class Test(TestCase):
-    def test_single_attack(self):
-        self.fail()
+    @patch("dnd.roll_die", side_effect=[20, 2])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch("dnd.roll_hitpoints", side_effect=[6])
+    def test_single_attack_hit_and_live(self, _, mock_stdout, __):
+        character = {"Name": "Xyxy",
+                     "Strength": 10,
+                     "Dexterity": 8,
+                     "Intelligence": 5,
+                     "Charisma": 9,
+                     "Wisdom": 7,
+                     "Constitution": 3,
+                     "Inventory": [],
+                     "XP": 0,
+                     "Class": "monk",
+                     "Race": "elf",
+                     "HP": [7, 7]}
+        sad_jim = {
+            "Name": "Sad Jim",
+            "Strength": 7,
+            "Dexterity": 8,
+            "Intelligence": 5,
+            "Charisma": 0,  # I'm aware this is impossible by normal means, but sad jim is just awful.
+            "Wisdom": 11,
+            "Constitution": 3,
+            "Inventory": [],
+            "XP": 0,
+            "Class": "loser",
+            "Race": "human",
+            "HP": [3, 3]
+        }
+        dnd.single_attack(character, sad_jim)
+        expected = """Sad Jim took the hit like a real champ but still took 2 damage!
+
+"""
+        self.assertEqual(expected, mock_stdout.getvalue())
