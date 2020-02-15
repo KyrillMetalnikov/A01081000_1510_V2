486c5ac2affbe8a2ebb922b94e58bde73fee7dd5
Tokeybee <tokeybees@gmail.com>
2020-02-14 16:30:30 -0800

    tested combat_round for player 2 to go first with retaliation. This completes All the unittests.

--- a2/test_dnd_combat_round.py
+++ a2/test_dnd_combat_round.py
@@ -118,3 +118,42 @@ Xyxy took the hit like a real champ but still took 2 damage!
 """
         dnd.combat_round(character, sad_jim)
         self.assertEqual(expected, mock_sysout.getvalue())
+
+    @patch("dnd.roll_for_initiative", side_effect=[False])
+    @patch('dnd.roll_die', side_effect=[20, 2, 20, 2])
+    @patch('dnd.roll_hitpoints', side_effect=[2, 2])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_combat_round_player_two_first_with_retaliation(self, mock_sysout, _, __, ___):
+        character = {"Name": "Xyxy",
+                     "Strength": 10,
+                     "Dexterity": 8,
+                     "Intelligence": 5,
+                     "Charisma": 9,
+                     "Wisdom": 7,
+                     "Constitution": 3,
+                     "Inventory": [],
+                     "XP": 0,
+                     "Class": "monk",
+                     "Race": "elf",
+                     "HP": [7, 7]}
+        sad_jim = {
+            "Name": "Sad Jim",
+            "Strength": 7,
+            "Dexterity": 8,
+            "Intelligence": 5,
+            "Charisma": 0,  # I'm aware this is impossible by normal means, but sad jim is just awful.
+            "Wisdom": 11,
+            "Constitution": 3,
+            "Inventory": [],
+            "XP": 0,
+            "Class": "loser",
+            "Race": "human",
+            "HP": [3, 3]
+        }
+        expected = """Xyxy took the hit like a real champ but still took 2 damage!
+
+Sad Jim took the hit like a real champ but still took 2 damage!
+
+"""
+        dnd.combat_round(character, sad_jim)
+        self.assertEqual(expected, mock_sysout.getvalue())
