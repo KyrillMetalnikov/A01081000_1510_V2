a1237ed9c3f13bc623966e41ea7606532bb47ab0
Tokeybee <tokeybees@gmail.com>
2020-02-26 16:24:29 -0800

    tested sparse_dot_product for unordered dictionaries and negatives

--- lab05/test_sparse_vector_sparse_dot_product.py
+++ lab05/test_sparse_vector_sparse_dot_product.py
@@ -17,3 +17,8 @@ class Test(TestCase):
         actual = sparse_vector.sparse_dot_product({0: 1, 5: 2, 6: 1, 'length': 8}, {1: 2, 3: 2, 6: 2, 'length': 8})
         expected = 2
         self.assertEqual(actual, expected)
+
+    def test_sparse_dot_product_unordered_with_negatives(self):
+        actual = sparse_vector.sparse_dot_product({4: -5, 'length': 5, 0: 4.3}, {'length': 5, 2: 7.5, 4: -6})
+        expected = 30
+        self.assertEqual(actual, expected)
