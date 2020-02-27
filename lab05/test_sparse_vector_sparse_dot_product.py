5cff833336caa5a28817344dd43f8816ec5d7007
Tokeybee <tokeybees@gmail.com>
2020-02-26 16:22:11 -0800

    tested sparse_dot_product for different length

--- lab05/test_sparse_vector_sparse_dot_product.py
+++ lab05/test_sparse_vector_sparse_dot_product.py
@@ -7,3 +7,8 @@ class Test(TestCase):
         actual = sparse_vector.sparse_dot_product({'length': 0}, {'length': 0})
         expected = 0
         self.assertEqual(actual, expected)
+
+    def test_sparse_dot_product_different_length(self):
+        actual = sparse_vector.sparse_dot_product({0: 1, 5: 2, 6: 1, 'length': 7}, {1: 2, 3: 2, 6: 2, 'length': 8})
+        expected = None
+        self.assertEqual(actual, expected)
