de57b937cbc873f6b65afd47c8427b6ea27a97d3
Tokeybee <tokeybees@gmail.com>
2020-03-05 14:00:25 -0800

    finished quiz06

--- lab06/quiz06.py
+++ lab06/quiz06.py
@@ -5,7 +5,7 @@ import time
 def timer(func):
     def wrapper_timer(*args, **kwargs):
         start_time = time.perf_counter()
-        value = func(*args, **kwargs)
+        func(*args, **kwargs)
         end_time = time.perf_counter()
         run_time = end_time - start_time
         print(f"finished {func.__name__!r} in {run_time:.4f} secs")
