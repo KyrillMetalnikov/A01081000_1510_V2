cfe61aab475b333c262c89fba9c8778d90e5131b
Tokeybee <tokeybees@gmail.com>
2020-03-20 15:30:57 -0700

    added docstrings to exceptions

--- lab07/exceptions.py
+++ lab07/exceptions.py
@@ -1,14 +1,25 @@
 """Demonstrate proper exception protocol."""
 
 
-def heron(number: int) -> float:
+def heron(number) -> float:
+    """
+    Find the square root of a number.
+
+    A function that uses heron's theorem to find the square root of a number.
+    :param number: A positive number (float or integer)
+    :precondition: number must be positive.
+    :postcondition: A close estimate of the square root will be found
+    :return: A clsoe estimate of the square root as a float.
+    """
     try:
         if number < 0:
             number / 0
     except ZeroDivisionError:
         print("Error: The number provided must be positive!")
         return -1
+
     square_root_guess = 1  # arbitrary value for the heron formula
+
     for _ in range(0, 1000):  # arbitrary amount of runs that get it close enough
         square_root_guess = (square_root_guess + (number / square_root_guess)) / 2
 
