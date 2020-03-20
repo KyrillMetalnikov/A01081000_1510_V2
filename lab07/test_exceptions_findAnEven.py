d1a449d33266ec059630463e2b9343add1b1dc21
Tokeybee <tokeybees@gmail.com>
2020-03-20 16:22:45 -0700

    tested findAnEven for a single even number in the list

--- lab07/test_exceptions_findAnEven.py
+++ lab07/test_exceptions_findAnEven.py
@@ -6,3 +6,8 @@ class Test(TestCase):
     def test_find_an_even_empty_list(self):
         with self.assertRaises(ValueError):
             exceptions.findAnEven([])
+
+    def test_find_an_even_one_even(self):
+        actual = exceptions.findAnEven([1, 4, 5, 9, 11])
+        expected = 4
+        self.assertEqual(actual, expected)
