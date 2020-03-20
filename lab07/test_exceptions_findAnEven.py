9fa8f9fb1b96755b91c6311715bfd52000e258b2
Tokeybee <tokeybees@gmail.com>
2020-03-20 16:23:52 -0700

    tested findAnEven for multiple evens

--- lab07/test_exceptions_findAnEven.py
+++ lab07/test_exceptions_findAnEven.py
@@ -11,3 +11,8 @@ class Test(TestCase):
         actual = exceptions.findAnEven([1, 4, 5, 9, 11])
         expected = 4
         self.assertEqual(actual, expected)
+
+    def test_find_an_even_multiple_evens(self):
+        actual = exceptions.findAnEven([1, 3, 2, 6, 2])
+        expected = 2
+        self.assertEqual(actual, expected)
