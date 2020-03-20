3329451e2e4269727c5042697d9cae11319f4139
Tokeybee <tokeybees@gmail.com>
2020-03-20 16:06:24 -0700

    added unittest for a negative number (expecting error)

--- lab07/test_exceptions_heron.py
+++ lab07/test_exceptions_heron.py
@@ -5,16 +5,20 @@ import math
 
 class Test(TestCase):
     def test_heron_perfect_root(self):
-        actual = 10.0
-        expected = exceptions.heron(100)
+        expected = 10.0
+        actual = exceptions.heron(100)
         self.assertEqual(actual, expected)
 
     def test_heron_float(self):
-        actual = math.sqrt(5.5)
-        expected = exceptions.heron(5.5)
+        expected = math.sqrt(5.5)
+        actual = exceptions.heron(5.5)
         self.assertAlmostEqual(actual, expected)
 
     def test_heron_non_perfect_int(self):
-        actual = math.sqrt(42)
-        expected = exceptions.heron(42)
+        expected = math.sqrt(42)
+        actual = exceptions.heron(42)
         self.assertAlmostEqual(actual, expected)
+
+    def test_heron_error(self):
+        actual = exceptions.heron(-1)
+        self.assertRaises(ZeroDivisionError)
