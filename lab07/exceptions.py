30e61bae7c953250a217f068e46cb078ad230b49
Tokeybee <tokeybees@gmail.com>
2020-03-20 15:22:15 -0700

    updated heron function to display an error upon a negative number being inputted as a parameter

--- lab07/exceptions.py
+++ lab07/exceptions.py
@@ -2,15 +2,22 @@
 
 
 def heron(number: int) -> float:
-    square_root_guess = 10  # arbitrary value for the heron formula
-    for _ in range(0, 25):  # arbitrary amount of runs that get it close enough
+    try:
+        if number < 0:
+            number / 0
+    except ZeroDivisionError:
+        print("Error: The number provided must be positive!")
+        return -1
+    square_root_guess = 1  # arbitrary value for the heron formula
+    for _ in range(0, 1000):  # arbitrary amount of runs that get it close enough
         square_root_guess = (square_root_guess + (number / square_root_guess)) / 2
 
     return square_root_guess
 
 
 def main():
-    print(heron(42))
+    print(heron(-5))
+    print(heron(3))
 
 
 if __name__ == "__main__":
