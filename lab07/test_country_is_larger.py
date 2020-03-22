802b05a86baa6c84406bea166fadda3574618964
Tokeybee <tokeybees@gmail.com>
2020-03-21 17:49:30 -0700

    tested is_larger for a False statement

--- lab07/test_country_is_larger.py
+++ lab07/test_country_is_larger.py
@@ -9,3 +9,10 @@ class TestCountry(TestCase):
         actual = canada.is_larger(denmark)
         expected = True
         self.assertEqual(actual, expected)
+
+    def test_is_larger_false(self):
+        canada = country.Country("Canada", 36290000, 9985000)
+        denmark = country.Country("Denmark", 5603000, 42933)
+        actual = denmark.is_larger(canada)
+        expected = False
+        self.assertEqual(actual, expected)
