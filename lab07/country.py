fb124f3860258b28eb1ca2e53c132cce45df6b0f
Tokeybee <tokeybees@gmail.com>
2020-03-21 17:04:29 -0700

    commented constructor function

--- lab07/country.py
+++ lab07/country.py
@@ -3,7 +3,16 @@
 
 class Country:
 
-    def __init__(self, name, population, area):
+    def __init__(self, name: str, population: int, area: int):
+        """
+        Initialize a new country.
+
+        :param name: A string representing the country's name.
+        :param population: An int representing the country's population.
+        :param area: An int representing the country's area in square kilometres.
+        :precondition: The data-types of the params are followed and area/population are > 0.
+        :postcondition: A country object will be initialized.
+        """
         self.name = name
         if population < 0:
             print("A population cannot be negative!")
