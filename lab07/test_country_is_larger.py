3814fc3c24dbee3f0079d6a4ca74d40a4fea98a7
Tokeybee <tokeybees@gmail.com>
2020-03-21 17:48:36 -0700

    tested is_larger for a true statement

--- lab07/test_country_is_larger.py
+++ lab07/test_country_is_larger.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
+import country
 
 
 class TestCountry(TestCase):
-    def test_is_larger(self):
-        self.fail()
+    def test_is_larger_true(self):
+        canada = country.Country("Canada", 36290000, 9985000)
+        denmark = country.Country("Denmark", 5603000, 42933)
+        actual = canada.is_larger(denmark)
+        expected = True
+        self.assertEqual(actual, expected)
