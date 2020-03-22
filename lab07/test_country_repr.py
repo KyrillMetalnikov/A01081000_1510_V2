ebd169338d292d873e2a9381cdbf027f039d2074
Tokeybee <tokeybees@gmail.com>
2020-03-21 20:27:29 -0700

    tested country_repr

--- lab07/test_country_repr.py
+++ lab07/test_country_repr.py
@@ -5,5 +5,7 @@ import country
 class TestCountry(TestCase):
     def test_country_repr(self):
         canada = country.Country("Canada", 36290000, 9985000)
-        canada
+        actual = canada.__repr__()
+        expected = 'Country("Canada", 36290000, 9985000)'
+        self.assertEqual(actual, expected)
 
