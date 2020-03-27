04f3633087744f4c257a37515e50d8c118500f63
Tokeybee <tokeybees@gmail.com>
2020-03-27 15:52:48 -0700

    tested tree for only negative age error

--- lab08/test_tree.py
+++ lab08/test_tree.py
@@ -19,3 +19,7 @@ class TestTree(TestCase):
     def test_tree_for_init_species_only_white_space(self):
         with self.assertRaises(ValueError):
             tree1 = tree.Tree("      ", 2, 2.0)
+
+    def test_tree_for_init_negative_age(self):
+        with self.assertRaises(ValueError):
+            tree1 = tree.Tree("Oak", -2, 2.0)
