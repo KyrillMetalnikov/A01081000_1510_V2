c9290279f44e3195701ece4ad4d35eb4e4979a36
Tokeybee <tokeybees@gmail.com>
2020-03-28 17:03:21 -0700

    added docstrings for factorial_iterative

--- lab09/recursion.py
+++ lab09/recursion.py
@@ -9,12 +9,27 @@ def timer(func):
         end_time = time.perf_counter()
         run_time = end_time - start_time
         with open("results.txt", "a") as file_object:
-            file_object.write(f"finished {func.__name__!r} in {run_time:.4f} secs")
+            file_object.write(f"finished {func.__name__!r} in {run_time:.10f} secs")
         return run_time
     return wrapper_timer
 
 
 def factorial_iterative(factorial):
+    """
+    Find the factorial.
+
+    :param factorial: A positive integer of which factorial you need to find.
+    :precondition: factorial will be a positive integer.
+    :postcondition: The factorial will be properly calculated.
+    :return: An integer representing the factorial
+
+    >>> factorial_iterative(0)
+    1
+    >>> factorial_iterative(3)
+    6
+    >>> factorial_iterative(6)
+    720
+    """
     total_value = 1
     if factorial == 0:
         return 1
