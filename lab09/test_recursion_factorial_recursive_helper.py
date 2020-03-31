5aaa6aab3b1fb12d660b2bf242346c25d8f792d9
Tokeybee <tokeybees@gmail.com>
2020-03-31 13:52:47 -0700

    tested recursive_helper for value 0 removed factorial_iterative test as testing is unviable

--- lab09/test_recursion_factorial_recursive_helper.py
+++ lab09/test_recursion_factorial_recursive_helper.py
@@ -7,3 +7,8 @@ class Test(TestCase):
         actual = recursion.factorial_recursive_helper(0)
         expected = 1
         self.assertEqual(actual, expected)
+
+    def test_factorial_recursive_helper_one(self):
+        actual = recursion.factorial_recursive_helper(1)
+        expected = 1
+        self.assertEqual(actual, expected)
