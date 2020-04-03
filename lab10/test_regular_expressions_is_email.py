cfd4fadb32fb8cf2207b21f57b42e0816ffd203d
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-03 10:53:02 -0700

    tested is_email for only numbers

--- lab10/test_regular_expressions_is_email.py
+++ lab10/test_regular_expressions_is_email.py
@@ -7,3 +7,9 @@ class Test(TestCase):
         actual = regular_expressions.is_email("Kyrill@hotmail.com")
         expected = True
         self.assertEqual(actual, expected)
+
+    def test_is_email_valid_numbers_only(self):
+        actual = regular_expressions.is_email("5432@3553.com")
+        expected = True
+        self.assertEqual(actual, expected)
+
