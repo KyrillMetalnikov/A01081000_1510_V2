7e6eb409cab33d377e4c3be4c81ff190c3241fcc
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-03 13:19:49 -0700

    tested is_nakamoto last name non capitalized

--- lab10/test_regular_expressions_is_nakamoto.py
+++ lab10/test_regular_expressions_is_nakamoto.py
@@ -13,7 +13,7 @@ class Test(TestCase):
         actual = regular_expressions.is_nakamoto("charles Nakamoto")
         self.assertEqual(expected, actual)
 
-    def test_is_nakamoto_correct_usage(self):
-        expected = True
-        actual = regular_expressions.is_nakamoto("Charles Nakamoto")
+    def test_is_nakamoto_invalid_last_name_not_capitalized(self):
+        expected = False
+        actual = regular_expressions.is_nakamoto("Charles nakamoto")
         self.assertEqual(expected, actual)
