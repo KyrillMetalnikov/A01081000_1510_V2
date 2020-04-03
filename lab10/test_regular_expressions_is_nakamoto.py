6580044dea918a46824eecf93b1d7df9552f0ede
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-03 13:19:15 -0700

    tested is_nakamoto first name non capitalized

---
+++ lab10/test_regular_expressions_is_nakamoto.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+import regular_expressions
+
+
+class Test(TestCase):
+    def test_is_nakamoto_correct_usage(self):
+        expected = True
+        actual = regular_expressions.is_nakamoto("Charles Nakamoto")
+        self.assertEqual(expected, actual)
+
+    def test_is_nakamoto_invalid_first_name_non_capitalized(self):
+        expected = False
+        actual = regular_expressions.is_nakamoto("charles Nakamoto")
+        self.assertEqual(expected, actual)
+
+    def test_is_nakamoto_correct_usage(self):
+        expected = True
+        actual = regular_expressions.is_nakamoto("Charles Nakamoto")
+        self.assertEqual(expected, actual)
