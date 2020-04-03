9c4cc735fa2f455a2563228c28c5833ce49700c6
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-03 14:23:37 -0700

    tested is_poker full house mix

--- lab10/test_regular_expressions_is_poker.py
+++ lab10/test_regular_expressions_is_poker.py
@@ -7,3 +7,13 @@ class Test(TestCase):
         expected = True
         actual = regular_expressions.is_poker("22223")
         self.assertEqual(expected, actual)
+
+    def test_is_poker_four_of_a_kind_letters(self):
+        expected = True
+        actual = regular_expressions.is_poker("ttttk")
+        self.assertEqual(expected, actual)
+
+    def test_is_poker_full_house_mix(self):
+        expected = True
+        actual = regular_expressions.is_poker("222jj")
+        self.assertEqual(expected, actual)
