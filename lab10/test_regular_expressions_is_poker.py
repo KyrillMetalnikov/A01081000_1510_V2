bc36c2fb90bc50cabb78794588a405df86f24d43
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-03 14:24:46 -0700

    tested is_poker two pairs

--- lab10/test_regular_expressions_is_poker.py
+++ lab10/test_regular_expressions_is_poker.py
@@ -22,3 +22,8 @@ class Test(TestCase):
         expected = True
         actual = regular_expressions.is_poker("222jk")
         self.assertEqual(expected, actual)
+
+    def test_is_poker_two_pair(self):
+        expected = True
+        actual = regular_expressions.is_poker("qqaa3")
+        self.assertEqual(expected, actual)
