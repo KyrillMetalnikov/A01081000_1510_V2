5edd907c4fa4f8f92d6fd036adf9d96f7d237ad8
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-11 16:21:14 -0700

    called weather api

--- a5/weather.py
+++ a5/weather.py
@@ -1,1 +1,22 @@
-#key = 7f1f5906c928af9f27256e0472282275
+"""Display the daily weather."""
+import json
+import requests
+# key = 7f1f5906c928af9f27256e0472282275
+# url=https://api.openweathermap.org/data/2.5/onecall?lat=49.2827&lon=123.1207&unit=metric&appid=
+
+
+def get_forecast(url: str) -> None:
+    response = requests.get(url)
+    response.raise_for_status()
+    vancouver_weather = json.loads(response.text)
+    print(vancouver_weather["current"])
+
+
+def main():
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=49.2827&lon=123.1207&unit=metric&appid"\
+          "=7f1f5906c928af9f27256e0472282275"
+    get_forecast(url)
+
+
+if __name__ == "__main__":
+    main()
