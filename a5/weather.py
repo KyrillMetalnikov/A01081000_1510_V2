0e59c15c9073e8a86a8ebdbcbd7b9f7bd68c8e68
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-11 18:28:05 -0700

    Converted time to PST and created loop

--- a5/weather.py
+++ a5/weather.py
@@ -1,8 +1,8 @@
 """Display the daily weather."""
 import json
 import requests
-# key = 7f1f5906c928af9f27256e0472282275
-# url=https://api.openweathermap.org/data/2.5/onecall?lat=49.2827&lon=123.1207&unit=metric&appid=
+import time
+import datetime
 
 
 def get_json(url: str) -> object:
@@ -11,10 +11,11 @@ def get_json(url: str) -> object:
     return json.loads(response.text)
 
 
-def display_weather(api_call: object) -> None:
-    print("Today the weather will be ", end="")
-    display_weather_description(api_call, 0)
-    display_min_max_temperature(api_call, 0)
+def display_weather(api_call: object, day: int) -> None:
+    print("The weather will be ", end="")
+    display_weather_description(api_call, day)
+    display_min_max_temperature(api_call, day)
+    display_sunrise_sunset(api_call, day)
 
 
 def display_weather_description(api_call: object, day: int) -> None:
@@ -27,13 +28,19 @@ def display_min_max_temperature(api_call: object, day: int) -> None:
 
 
 def display_sunrise_sunset(api_call: object, day: int) -> None:
-    print(f'Sunrise: {api_call["daily"][day]["sunrise"]}\n'
-          f'Sunset: {api_call["daily"][day]["sunset"]}')
+    print(f'Sunrise: {datetime.datetime.fromtimestamp(api_call["daily"][day]["sunrise"] - 28800)}\n'
+          f'Sunset: {datetime.datetime.fromtimestamp(api_call["daily"][day]["sunset"] - 28800)}')
+
 
 def main():
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=49.2827&lon=123.1207&units=metric&appid"\
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=49.2497&lon=-123.1193&units=metric&appid"\
           "=7f1f5906c928af9f27256e0472282275"
-    display_weather(get_json(url))
+    amount_of_days = input("How many days do you want to see? (up to 7, 0 for just today)")
+    try:
+        for i in range(0, int(amount_of_days) + 1):
+            display_weather(get_json(url), int(amount_of_days))
+    except ValueError:
+        print("Please input a number between 0 to 7 inclusive")
 
 
 if __name__ == "__main__":
