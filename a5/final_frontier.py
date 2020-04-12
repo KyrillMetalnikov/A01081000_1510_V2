ac0f8d5363dad21c1218e4dbf56dc48be4ffce48
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-11 21:35:46 -0700

    created loop in main with a 300 second sleep

--- a5/final_frontier.py
+++ a5/final_frontier.py
@@ -1,10 +1,19 @@
 from datetime import date
 import weather
+import time
 
 
 def display_near_earth_objects_velocity(api_call: dict, day: date):
-    print(api_call["near_earth_objects"][str(day)][0]["close_approach_data"][0]
-          ["relative_velocity"]["kilometers_per_hour"])
+    print(f'''The speed of this asteroid is {api_call["near_earth_objects"][str(day)][0]["close_approach_data"][0]
+    ["relative_velocity"]["kilometers_per_hour"]} kilometers per hour relative to the earth.\n''')
+
+
+def display_asteroid_name(api_call: dict, day: date):
+    print(f'There is an asteroid named {api_call["near_earth_objects"][str(day)][0]["name"]}.')
+
+
+def display_asteroid_size(api_call: dict, day: date):
+    print(f'This asteroid is also {api_call["near_earth_objects"][str(day)][0]["close_approach_data"][0]}')
 
 
 def get_tomorrows_date(todays_date: date):
@@ -16,7 +25,11 @@ def main():
     todays_date = date.today()
     url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date=&end_date=&api_key=" \
           f"KGgZUIGnLV5bsFf9auZsc7xFdHzgTTkzz5Alq6P8"
-    display_near_earth_objects_velocity(weather.get_json(url), get_tomorrows_date(todays_date))
+    # for i in range(0, 8):
+    #     display_asteroid_name(weather.get_json(url), get_tomorrows_date(todays_date))
+    #     display_near_earth_objects_velocity(weather.get_json(url), get_tomorrows_date(todays_date))
+    #     todays_date = get_tomorrows_date(todays_date)
+    #     time.sleep(300)
 
 
 if __name__ == "__main__":
