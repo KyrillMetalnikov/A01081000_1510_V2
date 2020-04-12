b7484c75e8283fb7bab98d0f45aff20d7c4c7447
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-11 20:22:32 -0700

    removed get_json and pulled it from other module

--- a5/final_frontier.py
+++ a5/final_frontier.py
@@ -1,23 +1,14 @@
 import json
 import requests
+import time
 import weather
 
 
-def get_json(url: str) -> dict:
-    """
-    Get and load the JSON object from url.
-
-    :param url: The url of the json object.
-    :precondition: The url will be a valid api address.
-    :postcondition: The function will get the json object from the address.
-    :return: A dict version of the json object.
-    """
-    response = requests.get(url)
-    response.raise_for_status()
-    return json.loads(response.text)
-
-
 def main():
-    url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=START_DATE&end_date=KGgZUIGnLV5bsFf9auZsc7x" \
-          "FdHzgTTkzz5Alq6P8"
-    print(get_json(url))
+    url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date=&end_date=&api_key=" \
+          f"KGgZUIGnLV5bsFf9auZsc7xFdHzgTTkzz5Alq6P8"
+    print(weather.get_json(url))
+
+
+if __name__ == "__main__":
+    main()
