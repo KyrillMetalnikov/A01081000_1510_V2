a1b0846db814e76278943c975c609f621cef50ea
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-11 19:44:14 -0700

    commented display_min_max_temperature

--- a5/weather.py
+++ a5/weather.py
@@ -4,14 +4,14 @@ import requests
 import datetime
 
 
-def get_json(url: str) -> object:
+def get_json(url: str) -> dict:
     """
     Get and load the JSON object from url.
 
     :param url: The url of the json object.
     :precondition: The url will be a valid api address.
     :postcondition: The function will get the json object from the address.
-    :return: A loaded JSON object.
+    :return: A dict version of the json object.
     """
     response = requests.get(url)
     response.raise_for_status()
@@ -22,8 +22,8 @@ def display_weather(api_call: dict, day: int) -> None:
     """
     Display the weather in vancouver for a set amount of days.
 
-    :param api_call: An openweatherapi json object.
-    :param day: An integer representing how many days to show.
+    :param api_call: An openweatherapi json object converted to dict.
+    :param day: An integer representing how many day to show.
     :precondition: The rules of the param must be followed with days being between 1-8.
     :postcondition: The weather will be displayed.
     """
@@ -34,12 +34,26 @@ def display_weather(api_call: dict, day: int) -> None:
 
 def display_weather_description(api_call: dict, day: int) -> None:
     """
-    Display the weather description
+    Display the weather description.
+
+    Displays if it is rainy, cloudy etc.
+    :param api_call: An openweatherapi json object converted to dict.
+    :param day: An integer representing which day to display.
+    :precondition: The rules of the param must be followed with day being between 1-8.
+    :postcondition: The weather description will be displayed.
     """
     print(api_call["daily"][day]["weather"][0]["main"])
 
 
 def display_min_max_temperature(api_call: dict, day: int) -> None:
+    """
+    Display the minimum and maximum temperature.
+
+    :param api_call: An openweatherapi json object converted to dict.
+    :param day: An integer representing which day to display.
+    :precondition: The rules of the param must be followed with day being between 1-8.
+    :postcondition: The weather description will be displayed.
+    """
     print(f'Minimum temperature: {api_call["daily"][day]["temp"]["min"]}\n'
           f'Maximum temperature: {api_call["daily"][day]["temp"]["max"]}')
 
