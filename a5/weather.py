895ce33020ff6f03e1f1db21a148f709f0ec01c7
Kyrill Metalnikov <tokeybees@gmail.com>
2020-04-11 17:25:56 -0700

    abstracted weather description

--- a5/weather.py
+++ a5/weather.py
@@ -12,11 +12,18 @@ def get_json(url: str) -> object:
 
 
 def display_weather(api_call: object) -> None:
-    print(api_call["daily"][0]["weather"][0]["main"])
+    print("Today the weather will be ", end="")
+    display_weather_description(api_call, 0)
+    print(f"With temperature fluctuations from {api_call['daily'][0]['temp']['min']} C "
+          f"to {api_call['daily'][0]['temp']['max']} C")
+
+
+def display_weather_description(api_call: object, day: int) -> None:
+    print(api_call["daily"][day]["weather"][0]["main"])
 
 
 def main():
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=49.2827&lon=123.1207&unit=metric&appid"\
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=49.2827&lon=123.1207&units=metric&appid"\
           "=7f1f5906c928af9f27256e0472282275"
     display_weather(get_json(url))
 
